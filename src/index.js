59bc2fd6fb97c3f4ea2f152e671342ab2d35973d
Aryan9060 <aryanofficial9060@gmail.com>
2025-06-08 12:41:03 +0530

    Add cookie-parser and cors ; implement ApiError and ApiResponse classes; create asyncHandler utility

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 // require("dotenv").config({ path: "./env" })
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
-dotenv.config({path:'./.env'})
+dotenv.config({ path: "./.env" })
+const port = process.env.PORT || 5000
 
 connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Database connection faild !!", error)
+    })
+    app.listen(port, () => {
+      console.log(`Server is running on port: ${port}`)
+    })
+  })
+  .catch((error) => console.error("Error connecting to MongoDB:", error))
 
 /*
 import mongoose from "mongoose"
